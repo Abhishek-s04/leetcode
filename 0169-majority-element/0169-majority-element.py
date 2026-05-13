7abe6c3fffa156a763842853ebabc8c4acf80f6d
Abhishek Kumar <abhisingh6200@gmail.com>
2026-05-13 10:31:28 +0530

    Time: 15 ms (28.81%), Space: 13.6 MB (73.69%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,17 +2,10 @@ class Solution(object):
     def majorityElement(self, arr):
 
         count_dict = {}
-        ans = 0
-        majority = arr[0]
-
         for item in arr:
-
             if item in count_dict:
                 count_dict[item] += 1
             else:
                 count_dict[item] = 1
-
-            if count_dict[item] > ans:
-                ans = count_dict[item]
-                majority = item
-        return majority
+            if count_dict[item] > len(arr)//2:
+                return item
